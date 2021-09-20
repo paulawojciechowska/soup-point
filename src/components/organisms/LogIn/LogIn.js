4c77768b3eade30bd25a7ef7d4403cb11b77946b
astropaula <wojciechowska.fran@gmail.com>
2021-09-20 16:05:36 +0200

    added redirections and display conditions

--- src/components/organisms/LogIn/LogIn.js
+++ src/components/organisms/LogIn/LogIn.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { NavLink, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { Input } from 'components/atoms/Input/Input';
 import { Header } from 'components/atoms/Header/Header';
 import { Button } from 'components/atoms/Button/Button';
@@ -13,25 +13,40 @@ const LogIn = () => {
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const { logIn } = useContext(dataContext);
+  const { logIn, user, logOut } = useContext(dataContext);
   let history = useHistory();
   const formSubmit = (data) => {
     logIn(data.email);
     history.push('/order');
   };
+  const handleLogout = () => {
+    logOut();
+    history.push('/');
+  };
   return (
-    <Wrapper as="form" onSubmit={handleSubmit(formSubmit)}>
-      <Header>Log in</Header>
-      <Input placeholder="*Email" {...register('email', { required: true })} />
-      <Input placeholder="*Password" {...register('password', { required: true })} />
-      <Button type="submit" orange>
-        Login
-      </Button>
-      <StyledParagraph>Don't have account yet?</StyledParagraph>
-      <Button grey onClick={() => history.push('/sign')}>
-        Create account
-      </Button>
-    </Wrapper>
+    <>
+      {user ? (
+        <Wrapper>
+          <Header>Here You can log out</Header>
+          <Button grey onClick={handleLogout}>
+            Log out
+          </Button>
+        </Wrapper>
+      ) : (
+        <Wrapper as="form" onSubmit={handleSubmit(formSubmit)}>
+          <Header>Log in</Header>
+          <Input placeholder="*Email" {...register('email', { required: true })} />
+          <Input type="password" placeholder="*Password" {...register('password', { required: true })} />
+          <Button type="submit" orange>
+            Login
+          </Button>
+          <StyledParagraph>Don't have account yet?</StyledParagraph>
+          <Button grey onClick={() => history.push('/sign')}>
+            Create account
+          </Button>
+        </Wrapper>
+      )}
+    </>
   );
 };
 
